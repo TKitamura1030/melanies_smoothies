86261ad122386ae3fe01a33260fe88978b2d6ae3
TKitamura1030 <kitamura-takahiro@fujielectric.com>
2024-10-01 18:15:45 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,13 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
     """Choose the fruits you want in your custom Smoothie!.
-    """
-)
+    """)
 
 import streamlit as st
 
@@ -23,15 +23,15 @@ my_dataframe = session.table("smoothies.public.FRUIT_OPTIONS").select(col('FRUIT
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients"
     ,my_dataframe
-    ,max_selections=5
-)
+    ,max_selections=5)
 
 if ingredients_list:
-
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -47,7 +47,5 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
+
